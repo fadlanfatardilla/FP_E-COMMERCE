d0350de168a30adf3d6493c6ec55d79da62c296c
Fadlanfatardillaaaa <fadlanfatardilla@gmail.com>
2024-06-01 21:18:09 +0700

    update-migration_model

---
+++ app/Models/Products.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'stock',
+        'size',
+        'category_id',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}

ad0e700337eca8e6574e6edb56eb65b2dff459d4
MohammadZakkyMubarok <mohzakkymub@gmail.com>
2024-06-27 17:23:30 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -11,13 +11,13 @@ use App\Http\Controllers\OrderItemController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\AuthController;
-
+    
 /*
 |--------------------------------------------------------------------------
 | API Routes
-|--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
+|--------------------------------------------------------------------------
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
